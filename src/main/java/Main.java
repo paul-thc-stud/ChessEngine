53ee11a288a786786f2503572b6b4c140999f1c4
Paul <108819816+paul-prog-stud@users.noreply.github.com>
2022-07-19 19:46:44 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,16 +6,5 @@ import Pieces.Queen;
 
 public class Main {
     public static void main(String[] args) {
-        Board b = new Board();
-        Piece pawn = new Pawn(PieceColor.WHITE);
-        Piece queen = new Queen(PieceColor.WHITE);
-        b.getBoard()[1][3] = null;
-        Point origin = new Point(3, 0);
-        Point dest = new Point(3, 5);
-        Move move = new Move(origin, dest, queen);
-        b.printBoard();
-        System.out.println("--------------");
-        move.execute(b);
-        b.printBoard();
     }
 }
